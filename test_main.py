2cd72e3df1b4c2b333f8c6a6a65bad6217d52135
IDNatte <idnatte@gmail.com>
2021-09-18 06:01:29 +0800

    fixing buildspec, test_main, and simple_jwt_api

--- test_main.py
+++ test_main.py
@@ -8,9 +8,9 @@ import pytest
 import main
 
 SECRET = os.getenv('JWT_SECRET')
-TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+TOKEN = os.getenv('JWT_TOKEN')
+EMAIL = os.getenv('TEST_EMAIL')
+PASSWORD = os.getenv('TEST_PASSWORD')
 
 @pytest.fixture
 def client():
